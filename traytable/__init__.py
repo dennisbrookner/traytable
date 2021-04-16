199e3f6c5d38d8da8c73b0dc4be22823224d8c0c
dennisbrookner <debrookner@gmail.com>
2021-04-16 17:17:12 -0400

    test push for versioning

--- traytable/__init__.py
+++ traytable/__init__.py
@@ -1,2 +1,9 @@
+def getVersionNumber():
+	import pkg_resources
+	version = pkg_resources.require("traytable")[0].version
+	return version
+
+__version__ = getVersionNumber()
+
 from .screens import screen, tray, clonetray, setrows, setcols
 from .wells import well
