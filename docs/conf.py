c77b18dee1865e1f83c4790af3e9d55e3b88169b
dennisbrookner <debrookner@gmail.com>
2021-04-16 13:19:37 -0400

    fiddling with docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../traytable'))
-import traytable as tt 
+#import traytable as tt 
 
 
 # -- Project information -----------------------------------------------------
