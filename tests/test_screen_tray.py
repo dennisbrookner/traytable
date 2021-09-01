b7f4eba53e726c2fc027d0e1867df48d353cf842
dennisbrookner <debrookner@gmail.com>
2021-08-31 21:18:38 -0400

    actually pushing tests this time

---
+++ tests/test_screen_tray.py
@@ -0,0 +1,40 @@
+import traytable as tt
+import pytest
+
+def test_screen_well():
+	screen = tt.screen(row='PEG', col='protein', maxwell='H6')
+	assert len(screen) == 4
+
+	tray = tt.tray(screen, rows=5, cols=6)
+
+	assert tray['C'] == 5
+	assert tray['3'] == 6
+	assert len(tray) == 18
+
+# this should be an error but isn't yet!!
+def test_bad_maxwell():
+	with pytest.raises(ValueError):
+		tt.screen(row='PEG', col='protein', maxwell='H3H')
+
+def test_gradients():
+	screen = tt.screen(row='PEG', col='protein', maxwell='H6')
+	tray = tt.tray(screen, rows=[1,8], cols=[10,15])
+	
+	assert tray['C'] == 3
+
+	assert tray['3'] == 12
+
+def test_assigned():
+	screen = tt.screen(row='PEG', col='protein', maxwell='H6')
+	tray = tt.tray(screen, rows=[1,2,3,4,5,7,7,8], cols=[10,11,12,14,14,15])
+	
+	assert tray['F'] == 7
+
+	assert tray['4'] == 14
+
+def test_bad_assigned():
+	screen = tt.screen(row='PEG', col='protein', maxwell='H6')
+	with pytest.raises(ValueError):
+		tt.tray(screen, rows=[1,2,3,4,5,7,7], cols=[10,11,12,14,14,15])
+
+
